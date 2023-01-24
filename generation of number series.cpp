c77aaf3a90e26c2326462f2d10016dc5ea9da17c
●────────vamsi亗 <112240971+vamsi788@users.noreply.github.com>
2023-01-24 13:23:00 +0530

    Add files via upload
    Enter the limit of a: 10
    
    12345678910
    --------------------------------
    Process exited after 4.26 seconds with return value 0
    Press any key to continue . . .

---
+++ generation of number series.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a, i;
+	printf("Enter the limit of a: ");
+	scanf("%d",&a);
+	printf("\n");
+	for(i=1; i<=a; i++)
+	{
+		if(i==a)
+		    printf("%d",i);
+		else
+		    printf("%d",i);
+	}
+	return 0;
+}
