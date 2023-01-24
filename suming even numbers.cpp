1d09d63aaf326b53997dd1ad9648868df0e11f84
●────────vamsi亗 <112240971+vamsi788@users.noreply.github.com>
2023-01-24 13:29:45 +0530

    Add files via upload
    Enter a value: 44
    2+4+6+8+10+12+14+16+18+20+22+24+26+28+30+32+34+36+38+40+42+44=506
    --------------------------------

---
+++ suming even numbers.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n, sum=0;
+	
+	printf("Enter a value: ");
+	scanf("%d",&n);
+	
+	for(int i=2; i<=n; i+=2)
+	{
+		printf("%d+",i);
+		sum += i;
+	}
+	
+	printf("\b=%d",sum);
+	
+	return 0;
+}
